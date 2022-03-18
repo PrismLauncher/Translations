8cdb8ebc565182c55f5ee1c84bcee039756bd92b
Sefa Eyeoglu <contact@scrumplex.net>
2022-03-18 13:09:47 +0100

    refactor: update .ts files directly

--- update.sh
+++ update.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 set -e
-shopt -s globstar
 
 ROOT="$(pwd)"
 SRC=${ROOT}/src
 
-LCONVERT_BIN=${LCONVERT_BIN:-lconvert}
-LRELEASE_BIN=${LRELEASE_BIN:-lrelease}
 LUPDATE_BIN=${LUPDATE_BIN:-lupdate}
 
 ###############################################################################
@@ -15,28 +12,19 @@ LUPDATE_BIN=${LUPDATE_BIN:-lupdate}
 readarray -d '' SOURCE_FILES < <(find "$SRC" -regex '.*\.\(h\|cpp\|ui\)' -type f -print0)
 
 update_file() {
-    po_file="$1"  # language.po
-    ts_file="$1.ts"  # language.po.ts
+    ts_file="$1"
 
-    echo "Updating $po_file..."
-
-    # Convert .po to .ts
-    $LCONVERT_BIN -locations relative "$po_file" -o "$ts_file"
+    echo "Updating $ts_file..."
 
     # Update .ts
     $LUPDATE_BIN "${SOURCE_FILES[@]}" -ts "$ts_file"
-
-    # Convert .ts to .po
-    $LCONVERT_BIN -locations relative "$ts_file" -o "$po_file"
-
-    rm -f "$ts_file"
 }
 
 cd "$ROOT"
 
-for f in *.po; do
+for f in *.ts; do
     update_file "$f"
 done
 
-update_file template.pot
+update_file .template.ts
 
