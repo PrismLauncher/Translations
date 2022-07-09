7bd7fad97d8b50993324f85187a24dca578d7517
雪鈴鈴 <yenhank0531@gmail.com>
2022-07-09 10:14:06 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 98.6% (1266 of 1283 strings)
    
    Translation: PolyMC/PolyMC
    Translate-URL: https://hosted.weblate.org/projects/polymc/polymc/zh_Hant/

--- zh_TW.ts
+++ zh_TW.ts
@@ -1440,7 +1440,7 @@ Please try again.</source>
         <location filename="src/launcher/ui/pages/modplatform/flame/FlamePage.cpp" line="162"/>
         <location filename="src/launcher/ui/pages/modplatform/flame/FlamePage.cpp" line="181"/>
         <source>No version is available!</source>
-        <translation type="unfinished"></translation>
+        <translation>沒有可用的版本！</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/modplatform/flame/FlamePage.cpp" line="235"/>
