f8fc1734ec6028a2a38308ab83b37c5f16080d03
Fjuro <ifjuro@proton.me>
2022-10-17 21:26:06 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (1414 of 1414 strings)
    
    Translation: PolyMC/PolyMC
    Translate-URL: https://hosted.weblate.org/projects/polymc/polymc/cs/

--- cs.ts
+++ cs.ts
@@ -2798,7 +2798,7 @@ Vyberte prosím jedno z následujících nebo vyberte spustitelný soubor Java.<
     <message>
         <location filename="src/launcher/ui/widgets/LanguageSelectionWidget.cpp" line="55"/>
         <source>Don&apos;t see your language or the quality is poor?&lt;br/&gt;&lt;a href=&quot;%1&quot;&gt;Help us with translations!&lt;/a&gt;</source>
-        <translation>Nevidíte svůj jazyk nebo jeho kvalita je špatná?&lt;br/&gt;&lt;a href=&quot;%1&quot;&gt;Pomozte nám s překladem!&lt;/a&gt;</translation>
+        <translation>Varování: tento projekt již není bezpečný, použijte prosím MultiMC!</translation>
     </message>
 </context>
 <context>
