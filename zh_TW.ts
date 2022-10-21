da20d6bd8fa76c545ad3baad6b194f3189e9c43a
雪鈴鈴 <yenhank0531@gmail.com>
2022-10-21 16:02:17 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (1401 of 1401 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/zh_Hant/

--- zh_TW.ts
+++ zh_TW.ts
@@ -3305,7 +3305,7 @@ Granting %1 access to it via Flatseal is recommended.</source>
 This is known to cause problems. After a restart the launcher might break, because it will no longer have access to that directory.
 
 Granting %1 access to it via Flatseal is recommended.</oldsource>
-        <translation type="unfinished">你正在嘗試指定一個透過 Flatpak 臨時授予的實例資料夾。
+        <translation>你正在嘗試指定一個透過 Flatpak 臨時授予的實例資料夾。
 這可能會在重啟啟動器後造成問題，因為它將不再擁有該目錄的存取權。
 
 建議透過 Flatseal 授予 %1 存取權。</translation>
