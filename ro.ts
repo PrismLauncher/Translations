2fa3c39d26b056d048c7da638e225ddc171ad137
MasterGamer64 <mastergameryt64@gmail.com>
2022-10-23 18:16:49 +0000

    Translated using Weblate (Romanian)
    Currently translated at 31.0% (435 of 1401 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/ro/

--- ro.ts
+++ ro.ts
@@ -7,7 +7,7 @@
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="39"/>
         <source>&amp;Pastebin Service</source>
         <oldsource>Pastebin Service</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Serviciu &amp;Pastebin</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="45"/>
