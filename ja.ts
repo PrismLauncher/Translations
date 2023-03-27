90e543f3872aafb2988195c616e2b63b2596c262
PrismAutomata <gitbot@scrumplex.net>
2023-03-27 06:04:25 +0000

    Update source strings

--- ja.ts
+++ ja.ts
@@ -1517,7 +1517,7 @@ Are you sure?</source>
     <message>
         <location filename="src/launcher/ui/pages/global/ExternalToolsPage.cpp" line="145"/>
         <source>JVisualVM Executable</source>
-        <translation>JVisualVM　実行ファイル</translation>
+        <translation>JVisualVM&#x3000;実行ファイル</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/ExternalToolsPage.cpp" line="153"/>
