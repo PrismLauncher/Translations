4abbeadf2f760e191c7a22f3d1a90aa219ca1779
PrismAutomata <gitbot@scrumplex.net>
2023-04-03 12:04:49 +0000

    Update source strings

--- pt.ts
+++ pt.ts
@@ -6163,7 +6163,7 @@ Please update %1!</source>
     <message>
         <location filename="src/launcher/ui/dialogs/OfflineLoginDialog.ui" line="42"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nome de usuário</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/OfflineLoginDialog.ui" line="49"/>
