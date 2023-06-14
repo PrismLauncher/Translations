39eef31b12434ecfaa069fefd12c558e01945aad
PrismAutomata <gitbot@scrumplex.net>
2023-06-14 00:11:53 +0000

    Update source strings

--- hu.ts
+++ hu.ts
@@ -5105,13 +5105,6 @@ Do you wish to proceed?</source>
 A törlés tönkreteheti a másik példányt,
 
 Szeretné folytatni?</numerusform>
-            <numerusform>A következő példányok hivatkozhatnak fájlokra a jelenlegi példányban:
-
-%1
-
-A törlés tönkreteheti a másik példányt,
-
-Szeretné folytatni?</numerusform>
         </translation>
     </message>
     <message>
