ac811bfde32e1346f6e001c0df462db3af77f5e2
razorloveskeke <ataberkzxc@gmail.com>
2023-07-06 13:36:31 +0000

    Translated using Weblate (Toki Pona)
    Currently translated at 27.5% (382 of 1387 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/tok/

--- tok.ts
+++ tok.ts
@@ -338,7 +338,7 @@
         <location filename="src/launcher/minecraft/auth/AccountList.cpp" line="320"/>
         <source>Errored</source>
         <comment>Account status</comment>
-        <translation>ike pi pali ala / ike pakala / ike</translation>
+        <translation>ike</translation>
     </message>
     <message>
         <location filename="src/launcher/minecraft/auth/AccountList.cpp" line="323"/>
