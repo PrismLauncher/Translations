f63f87fb626e1b46a1243bbe8a00eecca30061c7
Velah <velah027@gmail.com>
2023-09-08 17:26:54 +0000

    Translated using Weblate (Spanish (Uruguay))
    Currently translated at 61.8% (1057 of 1708 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/es_UY/

--- es_UY.ts
+++ es_UY.ts
@@ -3623,7 +3623,7 @@ AVISO: Asegurese de realizar una copia de seguridad de su instancia antes de act
     <message>
         <location filename="src/launcher/ui/pages/global/JavaPage.ui" line="191"/>
         <source>If enabled, the launcher will not check if an instance is compatible with the selected Java version.</source>
-        <translation type="unfinished"></translation>
+        <translation>Si está habilitado, el iniciador no verificará si una instancia es compatible con la versión de Java seleccionada.</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/JavaPage.ui" line="194"/>
