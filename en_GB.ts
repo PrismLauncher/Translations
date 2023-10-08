17fe17dd508086bc6180d1e5a1e616ad8fd1c2db
reece scott <reece.scott210505@gmail.com>
2023-10-08 20:13:47 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 99.7% (1677 of 1681 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/en_GB/

--- en_GB.ts
+++ en_GB.ts
@@ -6928,7 +6928,7 @@ Warning: This resource is hard linked elsewhere. Editing it will also change the
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="95"/>
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="112"/>
         <source>Remove mod&apos;s metadata</source>
-        <translation type="unfinished">Remove mod&apos;s metadata</translation>
+        <translation>Remove mod&apos;s metadata</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="109"/>
