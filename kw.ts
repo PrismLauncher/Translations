a537ece823afeb02a10a825cb14d9f9747899e71
PrismAutomata <gitbot@scrumplex.net>
2023-12-02 18:04:00 +0000

    Update source strings

--- kw.ts
+++ kw.ts
@@ -5104,6 +5104,7 @@ Deleting it could break the other instance(s),
 Do you wish to proceed?</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
     <message>
