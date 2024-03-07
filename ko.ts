4ae105b13299e89d331719bcf7a8c4cfe67d9b45
pji2918 <son77391@gmail.com>
2024-03-07 01:24:48 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (1867 of 1867 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/ko/

--- ko.ts
+++ ko.ts
@@ -3156,7 +3156,7 @@ NOTE: Make sure you made a backup of your important instance data before updatin
     <message>
         <location filename="src/launcher/ui/pages/modplatform/ImportPage.cpp" line="204"/>
         <source>Supported files</source>
-        <translation type="unfinished"></translation>
+        <translation>지원되는 파일</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/modplatform/ImportPage.cpp" line="207"/>
