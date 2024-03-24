d7df53470191c85aa1e647bf6e4ba660cbe9a510
marcin mikołajczak <me@mkljczk.pl>
2024-03-24 23:31:31 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (1867 of 1867 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/pl/

--- pl.ts
+++ pl.ts
@@ -3909,7 +3909,7 @@ Postępuj ostrożnie!</translation>
     <message>
         <location filename="src/launcher/InstanceList.cpp" line="890"/>
         <source>Could not create staging folder</source>
-        <translation type="unfinished">Nie można było stworzyć folderu przejściowego</translation>
+        <translation>Nie można utworzyć folderu przejściowego</translation>
     </message>
     <message>
         <location filename="src/launcher/InstanceList.cpp" line="908"/>
