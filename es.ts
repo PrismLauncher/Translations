192604262023b2ee481776905990bd0c38d4e880
Tixa El Nou <lucianoibarravallejos1@gmail.com>
2024-03-31 17:52:08 +0000

    Translated using Weblate (Spanish)
    Currently translated at 99.6% (1868 of 1874 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/es/

--- es.ts
+++ es.ts
@@ -6954,7 +6954,7 @@ Solo puede actualizarse mediante un archivo con formato %1
     <message>
         <location filename="src/launcher/ui/pages/global/MinecraftPage.ui" line="318"/>
         <source>Use Zink</source>
-        <translation>Usa el programa Ziñk.</translation>
+        <translation>Usa Zink</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/MinecraftPage.ui" line="117"/>
