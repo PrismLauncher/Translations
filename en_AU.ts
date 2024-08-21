2af99afd4411c502ce0a255eefebabf8d5127a97
Noah Smith <noahsmith2008.12@gmail.com>
2024-08-21 11:54:04 +0000

    Translated using Weblate (English (Australia))
    Currently translated at 100.0% (1974 of 1974 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/en_AU/

--- en_AU.ts
+++ en_AU.ts
@@ -6440,7 +6440,7 @@ This can be updated only using a file in %1 format
     <message>
         <location filename="src/launcher/meta/BaseEntity.cpp" line="121"/>
         <source>Loading local file</source>
-        <translation type="unfinished"></translation>
+        <translation>Loading local file</translation>
     </message>
 </context>
 <context>
@@ -6454,7 +6454,7 @@ This can be updated only using a file in %1 format
         <location filename="src/launcher/meta/Index.cpp" line="144"/>
         <source>Load meta for %1:%2</source>
         <comment>This is for the task name that loads the meta index.</comment>
-        <translation type="unfinished"></translation>
+        <translation>Load meta for %1:%2</translation>
     </message>
 </context>
 <context>
@@ -6463,7 +6463,7 @@ This can be updated only using a file in %1 format
         <location filename="src/launcher/meta/VersionList.cpp" line="37"/>
         <source>Load meta for %1</source>
         <comment>This is for the task name that loads the meta index.</comment>
-        <translation type="unfinished"></translation>
+        <translation>Load meta for %1</translation>
     </message>
 </context>
 <context>
