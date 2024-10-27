bd3fe1a1c8b311e40d90a12772cc49e26bc3781a
PrismAutomata <gitbot@scrumplex.net>
2024-10-27 00:14:43 +0000

    Update source strings

--- ja.ts
+++ ja.ts
@@ -7675,7 +7675,7 @@ This can be updated only using a file in %1 format
     <message>
         <location filename="src/launcher/ui/widgets/ModFilterWidget.cpp" line="127"/>
         <source>All Versions</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">すべてのバージョン</translation>
     </message>
 </context>
 <context>
