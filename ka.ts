61e745e3ab681befa2ba745cda2004ed7ef5167a
PrismAutomata <gitbot@scrumplex.net>
2024-10-29 12:06:18 +0000

    Update source strings

--- ka.ts
+++ ka.ts
@@ -3747,12 +3747,12 @@ Please make sure that the maximum memory value is lower.</source>
         <location filename="src/launcher/ui/java/InstallJavaDialog.cpp" line="289"/>
         <location filename="src/launcher/ui/java/InstallJavaDialog.cpp" line="320"/>
         <source>Install Java</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Java-ის დაყენება</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/java/InstallJavaDialog.cpp" line="315"/>
         <source>Could not determine Java download type!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Java-ის გადმოწერის ტიპის დადგენა ვერ ხერხდება!</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/java/InstallJavaDialog.cpp" line="316"/>
