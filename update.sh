632354962aa25029f86b23a84173903489c06ecd
yumu <93029942+yumunet@users.noreply.github.com>
2024-11-12 16:42:42 +0900

    feat: enable translations of stable version

--- update.sh
+++ update.sh
@@ -4,12 +4,13 @@ set -e
 
 ROOT="$(pwd)"
 SRC=${ROOT}/src
+SRC_STABLE=${ROOT}/src_stable
 
 LUPDATE_BIN=${LUPDATE_BIN:-lupdate}
 
 ###############################################################################
 
-readarray -d '' SOURCE_FILES < <(find "$SRC" -regex '.*\.\(h\|cpp\|ui\)' -type f -print0)
+readarray -d '' SOURCE_FILES < <(find "$SRC" "$SRC_STABLE" -regex '.*\.\(h\|cpp\|ui\)' -type f -print0)
 
 update_file() {
     ts_file="$1"
@@ -18,6 +19,9 @@ update_file() {
 
     # Update .ts
     $LUPDATE_BIN "${SOURCE_FILES[@]}" -locations "absolute" -ts "$ts_file"
+
+    # Remove all locations for stable
+    sed -i "/<location filename=\"src_stable/d" "$ts_file"
 }
 
 cd "$ROOT"
