7d41cfedf67c60fe91cca5c9917a5ed73e7fc48d
PrismAutomata <gitbot@scrumplex.net>
2024-12-04 18:05:32 +0000

    Update source strings

--- fy.ts
+++ fy.ts
@@ -6033,6 +6033,7 @@ Deleting it could break the other instance(s),
 Do you wish to proceed?</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
     <message>
