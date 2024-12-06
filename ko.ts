c1dd84b38f060a0197b61fc7a891377db51a8380
Perssphere <xphere07@gmail.com>
2024-12-06 13:34:09 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (2253 of 2253 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/ko/

--- ko.ts
+++ ko.ts
@@ -3440,7 +3440,7 @@ NOTE: Make sure you made a backup of your important instance data before updatin
     <message>
         <location filename="src/launcher/minecraft/auth/steps/GetSkinStep.cpp" line="12"/>
         <source>Getting skin.</source>
-        <translation>스킨 가져오는 중.</translation>
+        <translation>스킨을 가져오는 중입니다.</translation>
     </message>
     <message>
         <location filename="src/launcher/minecraft/auth/steps/GetSkinStep.cpp" line="35"/>
@@ -3497,7 +3497,7 @@ NOTE: Make sure you made a backup of your important instance data before updatin
     <message>
         <location filename="src/launcher/ui/dialogs/IconPickerDialog.cpp" line="39"/>
         <source>Search...</source>
-        <translation type="unfinished"></translation>
+        <translation>검색...</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/IconPickerDialog.cpp" line="74"/>
