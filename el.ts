fdc6218deab0736e7cf5ce842ffcbac6925218d1
Dimitrys Meliates <demetresmeliates@gmail.com>
2024-12-12 16:57:39 +0000

    Translated using Weblate (Greek)
    Currently translated at 32.8% (739 of 2253 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/el/

--- el.ts
+++ el.ts
@@ -755,7 +755,7 @@ Please update both your operating system and %1.</source>
     <message>
         <location filename="src/launcher/ui/widgets/ModFilterWidget.cpp" line="89"/>
         <source>All Versions</source>
-        <translation type="unfinished"></translation>
+        <translation>Όλες οι Εκδόσεις</translation>
     </message>
 </context>
 <context>
