28b1a9c3cce6364537e9a1ddb4d2d6d2a7f97a63
Maciej Dobrzański <maciejdbrznsk@gmail.com>
2025-01-02 16:48:24 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (2254 of 2254 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/pl/

--- pl.ts
+++ pl.ts
@@ -9404,7 +9404,7 @@ Uwaga: Upewnij się, że zrobiłeś kopię zapasową istotnych danych przed aktu
     <message>
         <location filename="src/launcher/modplatform/modrinth/ModrinthInstanceCreationTask.cpp" line="266"/>
         <source>The file &apos;%1&apos; is missing a download link. This is invalid in the pack format.</source>
-        <translation>W pliku &apos;%1&apos; brakuje link do pobrania. Jest to nieprawidłowe w formacie paczki.</translation>
+        <translation>W pliku &apos;%1&apos; brakuje linku do pobrania. Jest to błąd w formacie paczki.</translation>
     </message>
     <message>
         <location filename="src/launcher/modplatform/modrinth/ModrinthInstanceCreationTask.cpp" line="296"/>
