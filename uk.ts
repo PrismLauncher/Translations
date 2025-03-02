7a4992e335dafe115f0d63b8bc8ce10ae12adce0
cutiegin <gingabyt@gmail.com>
2025-03-02 22:22:02 +0100

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (2350 of 2350 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/uk/

--- uk.ts
+++ uk.ts
@@ -11012,12 +11012,13 @@ Choose your name carefully:</source>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="269"/>
         <source>Network Error: %1
 HTTP Status: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Помилка мережі: %1
+HTTP Статус: %2</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="281"/>
         <source>The server responded with the following error:</source>
-        <translation type="unfinished"></translation>
+        <translation>Сервер відповів з наступною помилкою:</translation>
     </message>
     <message>
         <source>The server returned the following error:</source>
