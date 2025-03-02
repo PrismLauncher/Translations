e0145904f382121372999d1f78af2b98edd07c28
Fjuro <fjuro@users.noreply.hosted.weblate.org>
2025-03-02 14:08:33 +0100

    Translated using Weblate (Czech)
    Currently translated at 100.0% (2350 of 2350 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/cs/

--- cs.ts
+++ cs.ts
@@ -11176,12 +11176,13 @@ Vyber si dobře svoje jméno:</translation>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="269"/>
         <source>Network Error: %1
 HTTP Status: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba sítě: %1
+Stav HTTP: %2</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="281"/>
         <source>The server responded with the following error:</source>
-        <translation type="unfinished"></translation>
+        <translation>Server odpověděl následující chybou:</translation>
     </message>
     <message>
         <source>The server returned the following error:</source>
