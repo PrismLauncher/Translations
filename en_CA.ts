f6fe84dc8ca4a2f03f17fe2c0d0cf0cd92baf337
Carson G <81428538+Cartrigger@users.noreply.github.com>
2025-03-04 05:06:16 +0100

    Translated using Weblate (English (Canada))
    Currently translated at 100.0% (2350 of 2350 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/en_CA/

--- en_CA.ts
+++ en_CA.ts
@@ -10153,12 +10153,13 @@ Choose your name carefully:</translation>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="269"/>
         <source>Network Error: %1
 HTTP Status: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Network Error: %1
+HTTP Status: %2</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="281"/>
         <source>The server responded with the following error:</source>
-        <translation type="unfinished"></translation>
+        <translation>The server responded with the following error:</translation>
     </message>
     <message>
         <source>The server returned the following error:</source>
