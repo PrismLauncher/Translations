02cdad7004eefabb749146af2922aa44b5eaacd0
Adonis Axis <unc0v3rgd@gmail.com>
2025-03-12 11:16:31 +0100

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (2350 of 2350 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/pt_BR/

--- pt_BR.ts
+++ pt_BR.ts
@@ -11228,12 +11228,13 @@ Escolha o seu nome com cuidado:</translation>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="269"/>
         <source>Network Error: %1
 HTTP Status: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro de internet: %1
+Erro HTTP: %2</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/ProfileSetupDialog.cpp" line="281"/>
         <source>The server responded with the following error:</source>
-        <translation type="unfinished"></translation>
+        <translation>O servidor respondeu com o seguinte erro:</translation>
     </message>
     <message>
         <source>The server returned the following error:</source>
