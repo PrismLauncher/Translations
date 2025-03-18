1dfb0da6bddd4068aea2be7e99a75cf291f9af36
Thomas Hintermeier <thomas.hintermeier@outlook.fr>
2025-03-18 12:35:30 +0100

    Translated using Weblate (French)
    Currently translated at 99.8% (2350 of 2353 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/fr/

--- fr.ts
+++ fr.ts
@@ -5214,17 +5214,17 @@ Vous pourrez changer la version de Java dans les paramètres plus tard.
     <message>
         <location filename="src/launcher/LaunchController.cpp" line="311"/>
         <source>The launcher&apos;s client identification has changed. Please remove &apos;%1&apos; and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">L&apos;identification du client a changé. Veuillez effacer&apos;%1&apos; et ré-essayer.</translation>
     </message>
     <message>
         <location filename="src/launcher/LaunchController.cpp" line="321"/>
         <source>&apos;%1&apos; no longer exists on the servers. It may have been migrated, in which case please add the new account you migrated this one to.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&apos;%1&apos; n&apos;existe plus sur les serveurs. Il a pu migrer, auquel cas veuillez ajouter le nouveau compte vers lequel vous avez migré.</translation>
     </message>
     <message>
         <location filename="src/launcher/LaunchController.cpp" line="338"/>
         <source>&apos;%1&apos; has expired and needs to be reauthenticated. Do you want to reauthenticate this account?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&apos;%1&apos; a expiré et a doit être de nouveau authentifié. Voulez-vous réauthentifier ce compte&#x202f;?</translation>
     </message>
     <message>
         <source>Your account is currently not logged in. Please enter your password to log in again. &lt;br /&gt; &lt;br /&gt; This could be caused by a password change.</source>
@@ -13322,7 +13322,7 @@ Version actuellement installée : %1</translation>
     <message>
         <location filename="src/launcher/ui/pages/instance/ServersPage.cpp" line="300"/>
         <source>Online</source>
-        <translation type="unfinished">En ligne</translation>
+        <translation>En ligne</translation>
     </message>
     <message>
         <source>Latency</source>
@@ -13394,7 +13394,7 @@ Version actuellement installée : %1</translation>
     <message>
         <location filename="src/launcher/ui/pages/instance/ServersPage.ui" line="182"/>
         <source>Refresh</source>
-        <translation type="unfinished">Actualiser</translation>
+        <translation>Actualiser</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/instance/ServersPage.h" line="65"/>
