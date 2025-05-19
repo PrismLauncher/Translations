f11348204b207e5a143fe7fb66959503c6920076
jia <1398832368@qq.com>
2025-05-19 04:43:11 +0200

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 100.0% (2423 of 2423 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/zh_Hans/

--- zh.ts
+++ zh.ts
@@ -21,7 +21,7 @@
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="45"/>
         <source>Paste Service &amp;Type</source>
         <oldsource>Paste Service Type</oldsource>
-        <translation>粘贴服务类型(&amp;T)</translation>
+        <translation>Pastebin 服务(&amp;P)</translation>
     </message>
     <message>
         <source>Base URL</source>
