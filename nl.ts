1996add4a3483c9cf7ff8526cc739ee0d7d3abde
nably bably <dylan.proost3@gmail.com>
2025-06-16 11:58:33 +0200

    Translated using Weblate (Dutch)
    Currently translated at 87.6% (2298 of 2623 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/nl/

--- nl.ts
+++ nl.ts
@@ -13025,7 +13025,7 @@ Wenst u door te gaan?</numerusform>
     <message>
         <location filename="src/launcher/GZip.cpp" line="203"/>
         <source>invalid compression level</source>
-        <translation type="unfinished"></translation>
+        <translation>ongeldig compressie niveau</translation>
     </message>
     <message>
         <location filename="src/launcher/GZip.cpp" line="205"/>
