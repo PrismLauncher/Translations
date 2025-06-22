a5e909c8eb1db7a06cbc356f66463079dd82cfc7
PrismAutomata <gitbot@scrumplex.net>
2025-06-22 06:06:13 +0000

    Update source strings

--- fur.ts
+++ fur.ts
@@ -13472,7 +13472,7 @@ Gjavâ pardabon?</translation>
     <message>
         <location filename="src/launcher/ui/setupwizard/ThemeWizardPage.h" line="37"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Aspiet</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/setupwizard/ThemeWizardPage.h" line="38"/>
