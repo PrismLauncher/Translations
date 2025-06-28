a26a9dfbe05842531baf4ca3e0467e20bb1ff391
Pankott <patrykkoczmara28@gmail.com>
2025-06-28 11:43:53 +0200

    Translated using Weblate (Polish)
    Currently translated at 92.9% (2438 of 2623 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/pl/

--- pl.ts
+++ pl.ts
@@ -1490,7 +1490,7 @@ Niektóre wersje Minecrafta mogą się nie załadować.
     <message>
         <location filename="src/launcher/ui/dialogs/BlockedModsDialog.ui" line="45"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Your configured global mods folder and default downloads folder are automatically checked for the downloaded mods and they will be copied to the instance if found.&lt;/p&gt;&lt;p&gt;Optionally, you may drag and drop the downloaded mods onto this dialog or add a folder to watch if you did not download the mods to a default location.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Click &apos;Open Missing&apos; to open all the download links in the browser. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Skonfigurowany folder globalnych modów i domyślny folder pobierania są automatycznie sprawdzane pod kątem pobranych modów i zostaną skopiowane do instalacji, jeśli zostaną znalezione.&lt;/p&gt;&lt;p&gt;Opcjonalnie, możesz przeciągnąć pobrane mody na to okienko dialogowe lub dodać folder do skanowania, jeśli nie pobrałeś modyfikacji do lokalizacji domyślnej.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Wybierz &apos;Otwórz brakujące&apos;, aby otworzyć wszystkie linki do pobrania w przeglądarce. &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/BlockedModsDialog.ui" line="59"/>
@@ -1928,7 +1928,7 @@ Niektóre wersje Minecrafta mogą się nie załadować.
     <message>
         <location filename="src/launcher/ui/dialogs/CreateShortcutDialog.ui" line="82"/>
         <source>Override the default account</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Zastąp konto domyślne</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/CreateShortcutDialog.ui" line="114"/>
