62c336adf95400ce8b443b9f5db975129e2ab722
Fjuro <git@alius.cz>
2025-06-28 21:04:56 +0200

    Translated using Weblate (Czech)
    Currently translated at 100.0% (2623 of 2623 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/cs/

--- cs.ts
+++ cs.ts
@@ -44,7 +44,7 @@
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="173"/>
         <source>&amp;Microsoft Authentication</source>
         <oldsource>&amp;Microsoft Authentation</oldsource>
-        <translation type="unfinished">&amp;Přihlášení pomocí Microsoftu</translation>
+        <translation>&amp;Přihlášení pomocí Microsoftu</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="228"/>
