3042a9b196e63b1b97282e232fab7d24b754f089
PrismAutomata <gitbot@scrumplex.net>
2025-06-30 18:06:29 +0000

    Update source strings

--- uk.ts
+++ uk.ts
@@ -15731,7 +15731,7 @@ Are you sure?</source>
     <message>
         <location filename="src/launcher/ui/setupwizard/ThemeWizardPage.h" line="37"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Вигляд</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/setupwizard/ThemeWizardPage.h" line="38"/>
