862b07a824a0aaf18b9af5765eee9550e5864c3d
PrismAutomata <gitbot@scrumplex.net>
2025-07-20 00:19:01 +0000

    Update source strings

--- lv.ts
+++ lv.ts
@@ -15098,7 +15098,7 @@ Are you sure?</source>
     <message>
         <location filename="src/launcher/ui/dialogs/VersionSelectDialog.cpp" line="72"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Labi</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/VersionSelectDialog.cpp" line="73"/>
