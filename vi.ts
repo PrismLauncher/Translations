35d73daca9591032ac3aaf19f5147e6789c800f0
nx0 <vut811337@gmail.com>
2025-07-30 16:25:16 +0200

    Translated using Weblate (Vietnamese)
    Currently translated at 88.8% (2337 of 2630 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/vi/

--- vi.ts
+++ vi.ts
@@ -875,7 +875,7 @@ Hãy cập nhật cả hệ điều hành của bạn và %1.</translation>
     <message>
         <location filename="src/launcher/ui/widgets/AppearanceWidget.ui" line="256"/>
         <source>Cat Opacity</source>
-        <translation>Độ mờ của chú mèo</translation>
+        <translation>Độ mờ của con mèo</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/widgets/AppearanceWidget.ui" line="311"/>
