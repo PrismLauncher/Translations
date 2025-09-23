3de8605296ce763763febb5d6240f14cf8a2bebb
notlin4 <iamnotlin4@gmail.com>
2025-09-23 10:48:43 +0200

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (2646 of 2646 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/zh_Hant/

--- zh_TW.ts
+++ zh_TW.ts
@@ -1142,7 +1142,7 @@ Some versions of Minecraft may not launch.
         <location filename="src/launcher/minecraft/update/AssetUpdateTask.cpp" line="80"/>
         <source>Getting the asset files from %1...</source>
         <oldsource>Getting the assets files from %1...</oldsource>
-        <translation type="unfinished">正在從 %1 取得資源檔案…</translation>
+        <translation>正在從 %1 取得資源檔案…</translation>
     </message>
     <message>
         <source>Getting the assets files from Mojang...</source>
