97a33def5591a35eb46d314f3aa47521e2bdb5cd
Simplmon <simplmon12@gmail.com>
2025-11-30 09:13:32 +0100

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (2660 of 2660 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/en_GB/

--- en_GB.ts
+++ en_GB.ts
@@ -7332,7 +7332,7 @@ Reason:
     <message>
         <location filename="src/launcher/archive/ExportToZipTask.cpp" line="47"/>
         <source>Could not add:</source>
-        <translation type="unfinished"></translation>
+        <translation>Could not add:</translation>
     </message>
     <message>
         <source>Could not create:</source>
@@ -7368,7 +7368,7 @@ Reason:
     <message>
         <location filename="src/launcher/archive/ExtractZipTask.cpp" line="108"/>
         <source>Failed to parse file %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to parse file %1</translation>
     </message>
     <message>
         <source>Failed to seek to first file in zip</source>
@@ -10362,12 +10362,12 @@ Are you sure?</translation>
     <message>
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="396"/>
         <source>Missing Mod Loader</source>
-        <translation type="unfinished"></translation>
+        <translation>Missing Mod Loader</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="397"/>
         <source>You need to install a compatible mod loader before installing mods. Would you like to do so?</source>
-        <translation type="unfinished"></translation>
+        <translation>You need to install a compatible mod loader before installing mods. Would you like to do so?</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="241"/>
