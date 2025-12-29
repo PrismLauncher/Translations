c7b149f2e0f914168ce3c233efde0fb40c16f0a7
Fjuro <fjuro@alius.cz>
2025-12-29 14:46:21 +0100

    Translated using Weblate (Czech)
    Currently translated at 100.0% (2662 of 2662 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/cs/

--- cs.ts
+++ cs.ts
@@ -3475,7 +3475,7 @@ Prosím zkus znovu.</translation>
     <message>
         <location filename="src/launcher/ui/pages/modplatform/import_ftb/ImportFTBPage.ui" line="22"/>
         <source>Note: Many recent FTB modpacks are also available from CurseForge! Also, if your FTB instances are not in the default location, select it using the button next to search.</source>
-        <translation type="unfinished"></translation>
+        <translation>Upozornění: Spousta novějších modpacků FTB je dostupná také na CurseForge! Pokud vaše instance FTB nejsou ve výchozím umístění, vyberte jej pomocí tlačítka vedle vyhledávání.</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/modplatform/import_ftb/ImportFTBPage.ui" line="34"/>
