1e7e3436d9f805fd78f23be8b985e88259ab8195
PrismAutomata <gitbot@scrumplex.net>
2025-12-31 12:08:35 +0000

    Update source strings

--- fil.ts
+++ fil.ts
@@ -14526,7 +14526,7 @@ Are you sure?</source>
     <message>
         <location filename="src/launcher/ui/dialogs/VersionSelectDialog.cpp" line="72"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ok</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/VersionSelectDialog.cpp" line="73"/>
