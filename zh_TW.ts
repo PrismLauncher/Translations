50031e0c6c3ffc93acddb8fd5fb1745b26d615fc
notlin4 <iamnotlin4@gmail.com>
2026-01-13 10:19:43 +0100

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (2250 of 2250 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/zh_Hant/

--- zh_TW.ts
+++ zh_TW.ts
@@ -1329,7 +1329,7 @@ Some versions of Minecraft may not launch.
     <message>
         <location filename="src/launcher/minecraft/auth/AuthFlow.cpp" line="97"/>
         <source>Preparing to log in...</source>
-        <translation type="unfinished"></translation>
+        <translation>正在準備登入…</translation>
     </message>
     <message>
         <location filename="src/launcher/minecraft/auth/AuthFlow.cpp" line="103"/>
