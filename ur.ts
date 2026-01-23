ad07742f9dafa5ebed67ddb795ecb4b02875e73f
PrismAutomata <gitbot@scrumplex.net>
2026-01-23 12:09:00 +0000

    Update source strings

--- ur.ts
+++ ur.ts
@@ -5973,6 +5973,7 @@ To clear the metadata cache manually, press Folders -&gt; View Launcher Root Fol
         <source> and its %n registered shortcut(s)</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
     <message>
@@ -9272,6 +9273,7 @@ New name: %2</source>
 
 Do you wish to proceed?</source>
         <translation type="unfinished">
+            <numerusform></numerusform>
             <numerusform></numerusform>
         </translation>
     </message>
