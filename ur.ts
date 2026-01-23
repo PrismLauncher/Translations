b306ee0ed85159af3d9d9ac8c1f516296fd54546
M Saim Afzal <shadowempergaming1997@gmail.com>
2026-01-23 14:44:53 +0100

    Translated using Weblate (Urdu)
    Currently translated at 0.5% (12 of 2262 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/ur/

--- ur.ts
+++ ur.ts
@@ -85,7 +85,7 @@
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="390"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Note: you only need to set this to access private data.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;۔ نوٹ: آپ کو صرف اس کو خفیہ ڈیٹا کی رسائی کیلئے لگانا چاہیے-  &lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;۔ نوٹ: آپ کو صرف اس کو خفیہ ڈیٹا کی رسائی کیلئے لگانا چاہیے- &lt;/p&gt;&lt;/body&gt;&lt;/html/&gt;</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="173"/>
