938b8fa2e8b2c5a08ee9fd9626a42a7258093314
Cxerizo <cxerizobirb@gmail.com>
2026-01-24 23:41:22 +0100

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (2262 of 2262 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/en_GB/

--- en_GB.ts
+++ en_GB.ts
@@ -67,7 +67,7 @@
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="205"/>
         <source>&amp;Microsoft Authentication</source>
         <oldsource>&amp;Microsoft Authentation</oldsource>
-        <translation>&amp;Microsoft Authentation</translation>
+        <translation>&amp;Microsoft Authentication</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="260"/>
