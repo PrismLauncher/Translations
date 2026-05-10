2ff5d30075fee9929e25659d3c20e4437e67da7b
teles <tech.estimator454@silomails.com>
2026-05-10 19:35:30 +0200

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 99.9% (2365 of 2367 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/pt_BR/

--- pt_BR.ts
+++ pt_BR.ts
@@ -51,7 +51,7 @@
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="132"/>
         <source>Refresh on launch</source>
-        <translation>Recarregar ao iniciar</translation>
+        <translation>Atualizar ao lançar</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="142"/>
@@ -2663,7 +2663,7 @@ Lançar mesmo assim? Isso pode causar lentidões no jogo e seu sistema.</transla
         <source>
 This instance cannot be launched because some libraries are missing or have not been downloaded yet. Please try again in online mode with a working Internet connection</source>
         <translation>
-Essa instância não pode ser iniciada porque algumas bibliotecas estão faltando ou não foram baixadas ainda. Por favor, tente novamente no modo online com conexão de Internet funcionando</translation>
+Esta instância não pode ser iniciada porque algumas bibliotecas estão faltando ou ainda não foram baixadas. Por favor tente novamente no modo online com uma conexão à Internet funcionando</translation>
     </message>
 </context>
 <context>
