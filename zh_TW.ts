f09626171b09ebf0f063a8a3ab0e01cabd64b8c2
Geeson Wan <wang14240@gmail.com>
2026-06-19 04:44:38 +0200

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (2376 of 2376 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/zh_Hant/

--- zh_TW.ts
+++ zh_TW.ts
@@ -7212,7 +7212,7 @@ Granting %1 access to it via Flatseal is recommended.</oldsource>
         <location filename="src/launcher/minecraft/launch/LauncherPartLaunch.cpp" line="167"/>
         <source>Could not launch Minecraft: %1</source>
         <extracomment>Error message displayed if instace can&apos;t start</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>無法啟動 Minecraft：%1</translation>
     </message>
     <message>
         <source>Could not launch Minecraft!</source>
@@ -7693,7 +7693,7 @@ Reason:
     <message>
         <location filename="src/launcher/LoggedProcess.cpp" line="117"/>
         <source>The process failed to start: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>處理程序啟動失敗：%1</translation>
     </message>
     <message>
         <source>The process failed to start.</source>
