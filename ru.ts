4f10100f4dea46866d9dfb4ae5eda21807636c0c
Artyom Rybakov <rib.artem@gmail.com>
2026-06-20 10:29:20 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2376 of 2376 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/ru/

--- ru.ts
+++ ru.ts
@@ -7261,7 +7261,7 @@ Granting %1 access to it via Flatseal is recommended.</oldsource>
         <location filename="src/launcher/minecraft/launch/LauncherPartLaunch.cpp" line="167"/>
         <source>Could not launch Minecraft: %1</source>
         <extracomment>Error message displayed if instace can&apos;t start</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Не удалось запустить Minecraft: %1</translation>
     </message>
     <message>
         <source>Could not launch Minecraft!</source>
@@ -7742,7 +7742,7 @@ Reason:
     <message>
         <location filename="src/launcher/LoggedProcess.cpp" line="117"/>
         <source>The process failed to start: %1</source>
-        <translation>«	»«</translation>
+        <translation>Не удалось запустить процесс: %1</translation>
     </message>
     <message>
         <source>The process failed to start.</source>
