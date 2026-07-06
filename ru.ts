85f800a17c027f7f50ee13f8456f076488f26e71
Artyom Rybakov <rib.artem@gmail.com>
2026-07-06 06:09:44 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2381 of 2381 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/ru/

--- ru.ts
+++ ru.ts
@@ -34,7 +34,7 @@
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="70"/>
         <source>Base &amp;URL</source>
-        <translation>База&amp;URL</translation>
+        <translation>Базовый &amp;URL-адрес</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="80"/>
