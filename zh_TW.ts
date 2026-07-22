8ae58800dd529cdf33b816bad556ade93426ffdf
Geeson Wan <wang14240@gmail.com>
2026-07-23 00:09:07 +0200

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (2346 of 2346 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/zh_Hant/

--- zh_TW.ts
+++ zh_TW.ts
@@ -11054,7 +11054,7 @@ Are you sure?</source>
     <message>
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="441"/>
         <source>No mod loader was installed. Please try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>未安裝模組載入器。請重試。</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/instance/ModFolderPage.cpp" line="264"/>
