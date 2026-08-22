4d97934f7b7e20a948838f7f3e12d152eb9f4be8
Geeson Wan <wang14240@gmail.com>
2026-08-22 22:48:53 +0200

    Translated using Weblate (Chinese (Traditional Han script))
    Currently translated at 100.0% (2411 of 2411 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/zh_Hant/

--- zh_TW.ts
+++ zh_TW.ts
@@ -15910,7 +15910,7 @@ Are you sure?</source>
     <message>
         <location filename="src/launcher/ui/dialogs/ReviewMessageBox.ui" line="46"/>
         <source>Resources</source>
-        <translation type="unfinished"></translation>
+        <translation>資源</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/dialogs/ReviewMessageBox.ui" line="65"/>
