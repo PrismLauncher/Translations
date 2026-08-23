d8a69d162cc3e145cbf589fe8f139357babcbf42
666 666 <bereznev666@gmail.com>
2026-08-23 18:51:51 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2411 of 2411 strings)
    
    Translation: Prism Launcher/Launcher
    Translate-URL: https://hosted.weblate.org/projects/prismlauncher/launcher/ru/

--- ru.ts
+++ ru.ts
@@ -34,7 +34,7 @@
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="70"/>
         <source>Base &amp;URL</source>
-        <translation>Базовый &amp;URL-адрес</translation>
+        <translation>123Базовый &amp;URL-адрес</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="80"/>
@@ -168,7 +168,7 @@
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="208"/>
         <source>User Agent</source>
-        <translation>User Agent</translation>
+        <translation>123User Agent</translation>
     </message>
     <message>
         <location filename="src/launcher/ui/pages/global/APIPage.ui" line="221"/>
